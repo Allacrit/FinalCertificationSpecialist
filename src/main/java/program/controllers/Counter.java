d668e86b6f2c834937c61dced412ecc068e3677b
Artem Oleynikov <allacrit@yandex.ru>
2024-03-14 21:27:26 +0300

    edit project

--- src/main/java/program/controllers/Counter.java
+++ src/main/java/program/controllers/Counter.java
@@ -1,17 +1,13 @@
 package program.controllers;
 
 public class Counter implements AutoCloseable {
-
     static int sum;
-
     {
         sum = 0;
     }
-
     public void add() {
         sum++;
     }
-
     @Override
     public void close() {
         System.out.println("Counter closed");
